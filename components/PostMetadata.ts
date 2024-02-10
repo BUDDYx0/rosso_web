f0796d612372bc6e477f7216db90a7c275ea87e5
chess <motionbusinessde@gmail.com>
2024-02-10 06:09:29 +0100

    Optimized images a little and renamed img folder to images due to complications

---
+++ components/PostMetadata.ts
@@ -0,0 +1,7 @@
+export interface PostMetadata {
+    title: string;
+    date: string;
+    subtitle: string;
+    type: string;
+    slug: string;
+}
--- src/app/components/HomeNews.tsx
+++ src/app/components/HomeNews.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import fs from 'fs';
+import matter from 'gray-matter';
+import {PostMetadata} from "../../../components/PostMetadata";
 
-const getPostMetadata = () => {
+const getPostMetadata = () : PostMetadata[] => {
     const folder = "src/posts";
     const files = fs.readdirSync(folder);
     const markdownPosts = files.filter((file) => file.endsWith(".md"));
-    const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-    return slugs;
+
+    const posts = markdownPosts.map((fileName) => {
+        const fileContents = fs.readFileSync(`src/posts/${fileName}`, "utf8");
+        const matterResult = matter(fileContents);
+        return {
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            subtitle: matterResult.data.subtitle,
+            type: matterResult.data.type,
+            slug: fileName.replace(".md", ""),
+        };
+    });
+
+    return posts;
 };
 
 const HomeNews = () => {
     const postMetadata = getPostMetadata();
-    const postPreviews = postMetadata.map((slug) => (
+    const postPreviews = postMetadata.map((post) => (
         // eslint-disable-next-line react/jsx-key
-        <Link href={`/posts/${slug}`}>
-            <div
-                className={'transition-all duration-300 w-80 rounded-lg bg-neutral-950 outline outline-2 outline-neutral-700 hover:shadow-2xl hover:drop-shadow-white-shadow hover:scale-95 hover:opacity-70 overflow-hidden\t'}>
+        <Link href={`/posts/${post.slug}`}>
+            <div className={'transition-all duration-300 w-80 h-96 max-h-96 relative rounded-lg bg-neutral-950 outline outline-2 outline-neutral-700 hover:shadow-2xl hover:drop-shadow-white-shadow hover:scale-95 hover:opacity-70 overflow-hidden'}>
                 <div>
                     <Image
-                        src="/images/ban_events.png"
+                        src={"/images/ban_events.png"}
                         alt={''}
                         width={700}
                         height={200}
@@ -29,18 +42,16 @@ const HomeNews = () => {
                     />
                 </div>
                 <div className={'p-4'}>
-                    <h5 className={'font-semibold text-lg'}>{slug}</h5>
-                    <p className={'text-neutral-400'}>1/15/2024 3:07 PM</p>
-                    <p className={''}>
-                        It's time to slaughter! Participate at our GM's hosted tournament and receive some juicy
-                        rewards!
-                    </p>
-                    <div className={'mt-3'}>
-                        <span className={'bg-rose-600 font-semibold p-1 rounded-lg'}>EVENT</span>
+                    <h5 className={'font-semibold text-lg'}>{post.title}</h5>
+                    <p className={'text-neutral-400'}>{post.date}</p>
+                    <p>{post.subtitle}</p>
+                    <div className={'mt-3 absolute bottom-4'}>
+                        <span className={'bg-rose-600 font-semibold p-1 rounded-lg'}>{post.type}</span>
                     </div>
                 </div>
             </div>
         </Link>
+
     ))
 
     return (
