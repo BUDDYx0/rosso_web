13f73a00d079accbe0e8f6acb853393604e934eb
chess <motionbusinessde@gmail.com>
2024-03-06 16:16:57 +0100

    All components now have the same backdrop blur

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -8,8 +8,7 @@ const getPostContent = (slug: string) => {
     const folder = "src/posts/";
     const file = `${folder}${slug}.md`;
     const content = fs.readFileSync(file, "utf8");
-    const matterResult = matter(content);
-    return matterResult;
+    return matter(content);
 };
 
 export const generateStaticParams = () => {
